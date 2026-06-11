15a28838e0a87ec8fb72a1168f3d4f35ad3799ea
MinerU Desktop <king@mineru-desktop.local>
2026-06-11 11:29:29 +0800

    v4.0.1: bat launcher replaces broken PyInstaller exe; conda env fixes

--- MinerU_CLI.bat
+++ MinerU_CLI.bat
@@ -1,4 +1,12 @@
 @echo off
 cd /d "%~dp0"
-call %USERPROFILE%\.conda\envs\MinerU\python.exe scripts\mineru_cli.py
+echo MinerU CLI v4.0.1
+echo.
+for %%d in ("%USERPROFILE%\.conda\envs\MinerU" "C:\ProgramData\miniconda3\envs\MinerU") do (
+    if exist "%%~d\python.exe" (
+        "%%~d\python.exe" "%~dp0scripts\mineru_cli.py"
+        exit /b
+    )
+)
+echo MinerU conda environment not found!
 pause
